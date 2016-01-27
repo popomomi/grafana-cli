b84bb19de5ff1eaaa01d54046bd1757c7449325e
bergquist <carl.bergquist@gmail.com>
2016-01-27 18:55:08 +0100

    feat(godep): initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "Grafana cli"
+	app.Author = "Grafana Contributors"
+	app.Email = "https://github.com/grafana/grafana"
+
+}
