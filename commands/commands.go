0e269f849567b98839e5b70e37d0881ac2bd5c09
bergquist <carl.bergquist@gmail.com>
2016-01-27 19:51:02 +0100

    move commands into seperate package

---
+++ commands/commands.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"github.com/codegangsta/cli"
+)
+
+var Commands = []cli.Command{
+	{
+		Name:  "install",
+		Usage: "installs stuff",
+		Action: func(c *cli.Context) {
+			println("installllah")
+		},
+	}, {
+		Name:  "upgrade",
+		Usage: "upgrades stuff",
+		Action: func(c *cli.Context) {
+			println("up up och iväg!")
+		},
+	}, {
+		Name:  "remove",
+		Usage: "removes stuff",
+		Action: func(c *cli.Context) {
+			println("nice and tidy!")
+		},
+	}, {
+		Name:  "upgrade",
+		Usage: "upgrades stuff",
+		Action: func(c *cli.Context) {
+			println("up up och iväg!")
+		},
+	},
+}
--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	commands "github.com/grafana/grafana-cli/commands"
+	"os"
 )
 
 func main() {
 
 	app := cli.NewApp()
 	app.Name = "Grafana cli"
-	app.Author = "Grafana Contributors"
+	app.Author = "raintank"
 	app.Email = "https://github.com/grafana/grafana"
+	app.Commands = commands.Commands
 
+	app.Run(os.Args)
 }
